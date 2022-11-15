71fe3e73a3215bb459e7d2750c852d3f47fab8ba
Matt <39172048+magiccreb@users.noreply.github.com>
2022-11-15 11:14:16 -0500

    time blocks established. created main.js and added scoping commentary. timeblock class added to style.css

---
+++ Develop/main.js
@@ -0,0 +1,12 @@
+/* 11/15/2022
+Todo:
+- set up functions/listeners for event entry
+- set up local storage to hold entered events
+- 
+
+
+*/
+
+// when I click into a given hour's text area I need to be able to enter text that I can save to the hour by clicking the save button to the right. these events are stored locally.
+
+// text areas should have different colors for past, present, and future hours. these classes are pre-defined as .past, .present, and .future
