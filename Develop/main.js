768fde15ff401a37ffb84943a01f9778d620db6a
Matt <39172048+magiccreb@users.noreply.github.com>
2022-11-16 07:13:25 -0500

    updates to html. set up date display. additional stylization updates.

--- Develop/main.js
+++ Develop/main.js
@@ -7,6 +7,14 @@ Todo:
 
 */
 
+// variable to hold today's date in the desired format
+
+var today = moment().format('dddd') + ", " + moment().format('MMMM Do') + ", " + moment().format('YYYY');
+
+// sets the text of the #currentDay paragraph to the current date
+
+$('#currentDay').text(today);
+
 // when I click into a given hour's text area I need to be able to enter text that I can save to the hour by clicking the save button to the right. these events are stored locally.
 
 // text areas should have different colors for past, present, and future hours. these classes are pre-defined as .past, .present, and .future
