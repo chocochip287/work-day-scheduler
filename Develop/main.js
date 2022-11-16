d6e8fb23bc3ec7d7c6325b5b735f0a7a2d94eb11
Matt <39172048+magiccreb@users.noreply.github.com>
2022-11-16 12:27:00 -0500

    validated time style functionality so I removed the test time block from index.html. added loop to set up event listeners for the save buttons and set it up to run on page load.

--- Develop/main.js
+++ Develop/main.js
@@ -7,7 +7,9 @@ Todo:
 
 */
 
-var now;
+// variable for event testing
+
+var doc = document;
 
 // variable to reference button elements for event handling/loops
 
@@ -21,25 +23,21 @@ var textAreas = document.getElementsByTagName("textarea");
 
 var today = moment().format('dddd') + ", " + moment().format('MMMM Do') + ", " + moment().format('YYYY');
 
-// 
-
 // sets the text of the #currentDay paragraph to the current date
 
 $('#currentDay').text(today);
 
-// when I click into a given hour's text area I need to be able to enter text that I can save to the hour by clicking the save button to the right. these events are stored locally.
-
-// text areas should have different colors for past, present, and future hours. these classes are pre-defined as .past, .present, and .future.
-
-// loop to style the text areas based on the area's assigned hour compared with the actual time
+// function to set style for textareas
 
 function textareaStyler() {
     // need to add/remove classes based on the hour - compare current hour to its related time block ID
     for (i = 0; i < textAreas.length; i++) {
+        // comparison variable for the current hour
         var now = moment().hour();
+        // logic to determine text area sylization
         if (textAreas[i].id > now) {
             $(textAreas[i]).addClass('future');
-        } else if (textAreas[i].id === now) {
+        } else if (textAreas[i].id == now) {
             $(textAreas[i]).addClass('present');
         } else {
             $(textAreas[i]).addClass('past');
@@ -47,4 +45,26 @@ function textareaStyler() {
     }
 }
 
+// sets textarea stylization on page load
 textareaStyler();
+
+// button event listener assignment loop
+function buttonListener() {
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", buttonTester)
+    }
+}
+
+// establishes the button listeners for all buttons on page load
+
+buttonListener();
+
+function buttonTester(event) {
+    for (i = 0; i < buttons.length; i++) {
+        if (event.target === buttons[i]) {
+            console.log("yeah you're clicking a button");
+        } else {
+            console.log("you're clickin' something but not a button")
+        }
+    }   
+}
